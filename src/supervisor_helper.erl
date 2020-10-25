40d9d0a7086ebfbcccbded8cdabac358d234eb7b
Joakim G <joagre@gmail.com>
2020-10-25 18:52:14 +0100

    Added a supervisor_helper module to make it less error prone to shared worker pids between siblings

---
+++ src/supervisor_helper.erl
@@ -0,0 +1,32 @@
+-module(supervisor_helper).
+-export([foreach_worker/2, get_selected_worker_pids/2]).
+
+%% Exported: foreach_worker
+
+foreach_worker(SupervisorPid, Do) ->
+    Workers =
+        lists:foldl(fun({Id, Pid, worker, _}, Acc) ->
+                            [{Id, Pid}|Acc];
+                       (_, Acc) ->
+                            Acc
+                    end, [], supervisor:which_children(SupervisorPid)),
+    foreach_worker(Do, Workers, Workers).
+
+foreach_worker(_Do, _Workers, []) ->
+    ok;
+foreach_worker(Do, Workers, [{Id, Pid}|Rest]) ->
+    Do(Id, Pid, lists:keydelete(Id, 1, Workers)),
+    foreach_worker(Do, Workers, Rest).
+
+%% Exported: get_selected_worker_pids
+
+get_selected_worker_pids([], NeighbourWorkers) ->
+    [];
+get_selected_worker_pids([Id|Rest], NeighbourWorkers) ->
+    case lists:keytake(Id, 1, NeighbourWorkers) of
+        {value, {Id, Pid}, RemainingNeighbourWorkers} ->
+            [Pid|get_selected_worker_pids(
+                   Rest, lists:delete(Id, NeighbourWorkers))];
+        false ->
+            [undefined|get_selected_worker_pids(Rest, NeighbourWorkers)]
+    end.
